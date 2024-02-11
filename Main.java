429ea048324a8a672ddf2fd518d56453d7351592
BongaVelem <bongavelem@outlook.com>
2024-02-12 00:28:36 +0200

    Completed scanner class

--- Main.java
+++ Main.java
@@ -1,31 +1,60 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    String empName;
-    String empSurname;
-    String empNumber;
-    double empSalary;
-    String userInput;
-    double newSalary;
-    double increasePercentage;
+    Scanner scanner = new Scanner(System.in);
 
-    empName = JOptionPane.showInputDialog("Enter employee name: ");
-    empSurname = JOptionPane.showInputDialog("Enter employee Surname: ");
-    empNumber = JOptionPane.showInputDialog("Enter employee Number: ");
-    userInput = JOptionPane.showInputDialog("Enter employee Salary: ");
-    empSalary = Double.parseDouble(userInput);
-    userInput = JOptionPane.showInputDialog("Enter the percentage increase: ");
-    increasePercentage = Double.parseDouble(userInput);
-    newSalary = empSalary + (empSalary * (increasePercentage / 100));
+    System.out.print("Name: ");
+    String name = scanner.nextLine();
 
-    JOptionPane.showMessageDialog(null, "Employee Name:" + empName +
-        "\nEmployee Surname: " + empSurname +
-        "\nEmployee Number: " + empNumber +
-        "\nEmployee Salary: R" + empSalary +
-        "\nNew Salary: R" + newSalary);
+    System.out.print("Surname: ");
+    String surname = scanner.nextLine();
 
-    Employee employeeInstanceClass = new Employee();
+    System.out.print("Number: ");
+    String number = scanner.nextLine();
+
+    System.out.print("Salary: ");
+    Double salary = scanner.nextDouble();
+
+    System.out.print("Salary Increase % : ");
+    Double salaryIncrease = scanner.nextDouble();
+
+    Employee employee1 = new Employee(name, surname, number, salary);
+
+    System.out.print(" ");
+    System.out.println("=================Results 1: ===========");
+    System.out.print(" ");
+    employee1.increaseSalary(salaryIncrease);
+    System.out.println(employee1.toString());
+    System.out.println();
+    System.out.println();
+
+    Scanner scanner1 = new Scanner(System.in);
+
+    System.out.print("Name2: ");
+    String name2 = scanner1.nextLine();
+
+    System.out.print("Surname2: ");
+    String surname2 = scanner1.nextLine();
+
+    System.out.print("Number2: ");
+    String number2 = scanner1.nextLine();
+
+    System.out.print("Salary2: ");
+    Double salary2 = scanner1.nextDouble();
+
+    System.out.print("Salary Increase2 % : ");
+    Double salaryIncrease2 = scanner1.nextDouble();
+
+    Employee employee2 = new Employee(name2, surname2, number2, salary2);
+
+    System.out.print(" ");
+    System.out.println("=================Results 2: ===========");
+    System.out.print(" ");
+    employee2.increaseSalary(salaryIncrease2);
+    System.out.println(employee2.toString());
+
+    scanner.close();
+    scanner1.close();
   }
-
 }
